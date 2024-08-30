aae16cbc0f6ce9c220ae92f63e20bcb40fdbe212
Murilojms7 <murilojms7@gmail.com>
2024-08-30 09:36:14 -0300

    project-cars2

--- src/app/actions/car-actions.ts
+++ src/app/actions/car-actions.ts
@@ -1,14 +1,15 @@
 "use server"
 
-export async function fetchCars(){
+export async function fetchCars() {
     const response = await fetch("http://localhost:8080/cars", {
         cache: 'no-store'
     })
     const cars = await response.json()
     return cars
+
 }
 
-export async function fetchCarById(id: string){
+export async function fetchCarById(id: string) {
     const response = await fetch(`http://localhost:8080/cars/${id}`, {
         cache: 'no-store'
     })
